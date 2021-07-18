f180d65cde781d7678d84cc5adb7176d9cdd543f
Nishan Siriwardena <nishan.siriwardena@xero.com>
2021-07-18 19:47:53 +1000

    Main execution path

---
+++ ExchangeRates.Processor/ExchangeRatesImporter.cs
@@ -0,0 +1,48 @@
+using ExchangeRates.Processor.Services;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public interface IExchangeRateImporter
+{
+    Task Process();
+}
+
+public class ExchangeRateImporter : IExchangeRateImporter
+{
+    private readonly IExchangeRateCacheService _cacheService;
+    private readonly IEventNotificationService _notificationService;
+    private readonly IExchangeRateServiceClient _serviceClient;
+    private readonly IExchangeRateDataService _dataService ;
+
+
+    public ExchangeRateImporter(IExchangeRateDataService dataService, IExchangeRateCacheService cacheService, IEventNotificationService notificationService, IExchangeRateServiceClient serviceClient)
+    {
+        _dataService = dataService;
+        _cacheService = cacheService;
+        _notificationService = notificationService;
+        _serviceClient = serviceClient;
+    }
+
+    public async Task Process()
+    {
+        var rates = await _serviceClient.GetRates();
+        var changedRates = await _dataService.GetChangedRates(rates);
+
+        if(changedRates == null)
+        {
+            await _dataService.SaveLastUpdatedDate(rates);
+            return;
+        }
+
+        var tasks = new List<Task>
+        {
+            _dataService.SaveNewRates(changedRates),
+            _cacheService.AddToCache(changedRates),
+            _notificationService.Notify(changedRates),
+        };
+
+        await Task.WhenAll(tasks);
+    }
+}
+
