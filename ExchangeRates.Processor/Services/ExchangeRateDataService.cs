da6f8265191bf284902f4dcd6ed3a4441fa595eb
Nishan Siriwardena <nishan.siriwardena@xero.com>
2021-07-18 19:47:04 +1000

    Data service abstraction

--- ExchangeRates.Processor/Services/ExchangeRateDataService.cs
+++ ExchangeRates.Processor/Services/ExchangeRateDataService.cs
@@ -1,5 +1,35 @@
 using System;
-public interface IExchangeRateDataService
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ExchangeRates.Processor.Services
 {
-    
+    public interface IExchangeRateDataService
+    {
+        Task<ExchangeRatesDto> GetChangedRates(object rates);
+        Task SaveLastUpdatedDate(object rates);
+        Task SaveNewRates(object changedRates);
+    }
+
+    public class ExchangeRateDataService : IExchangeRateDataService
+    {
+        public Task<ExchangeRatesDto> GetChangedRates(object rates)
+        {
+            // Check the timestamp to see if it's newer.
+            return Task.FromResult(new ExchangeRatesDto
+            {
+                Rates = new Dictionary<string, double>()
+            });
+        }
+
+        public Task SaveLastUpdatedDate(object rates)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveNewRates(object changedRates)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
