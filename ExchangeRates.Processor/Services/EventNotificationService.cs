54ba6407790140729df7d3e9edfda384027a7a5c
Nishan Siriwardena <nishan.siriwardena@xero.com>
2021-07-18 19:46:09 +1000

    Abstractions for services to be used later

---
+++ ExchangeRates.Processor/Services/EventNotificationService.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+namespace ExchangeRates.Processor.Services
+{
+    public interface IEventNotificationService
+    {
+        Task Notify(object changedRates);
+    }
+
+    public class EventNotificationService : IEventNotificationService
+    {
+        public Task Notify(object changedRates)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ ExchangeRates.Processor/Services/ExchangeRateCacheService.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace ExchangeRates.Processor.Services
+{
+    public interface IExchangeRateCacheService
+    {
+        Task AddToCache(object changedRates);
+    }
+    public class ExchangeRateCacheService : IExchangeRateCacheService
+    {
+        public Task AddToCache(object changedRates)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
