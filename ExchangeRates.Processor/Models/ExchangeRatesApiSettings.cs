a854074a9fc2a63f40f8c761d2aadb95402e588d
Nishan Siriwardena <nishan.siriwardena@xero.com>
2021-07-18 19:43:48 +1000

    Set up DI and configuration

---
+++ ExchangeRates.Processor/Models/ExchangeRatesApiSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace ExchangeRates.Processor.Services
+{
+    public class ExchangeRatesApiSettings
+    {
+        public string AppId { get; set; }
+        public string Url { get; set; }
+    }
+
+    public class AppSettings
+    {
+        public string TempDirectory { get; set; }
+    }
+}
--- ExchangeRates.Processor/Program.cs
+++ ExchangeRates.Processor/Program.cs
@@ -1,7 +1,10 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using ExchangeRates.Processor.Services;
+using Microsoft.Extensions.Configuration;
+using ExchangeRates.Processor.Lib;
+
 
 namespace ExchangeRates.Processor
 {
@@ -9,15 +12,32 @@ namespace ExchangeRates.Processor
     {
         static async Task Main(string[] args)
         {
-            var builder = new HostBuilder()
-                .ConfigureServices((context, services) =>{
-                    services.AddTransient<IExchangeRateProcessingService, ExchangeRateProcessingService>();
-                    
+            var host = new HostBuilder()
+                .ConfigureAppConfiguration(configuration=> 
+                {
+                    configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: false);
+                    configuration.Build();
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddOptions();
+
+                    services.Configure<ExchangeRatesApiSettings>(context.Configuration.GetSection("ExchangeRatesApiSettings"));
+                    services.AddHttpClient<IHttpClientService, HttpClientService>();
+                    services.AddTransient<IHttpClientService, HttpClientService>();
+                    services.AddTransient<IJsonSerializer, JsonSerializerService>();
+
+                    services.AddTransient<IExchangeRateImporter, ExchangeRateImporter>();
+                    services.AddTransient<IExchangeRateDataService, ExchangeRateDataService>();
+                    services.AddTransient<IExchangeRateCacheService, ExchangeRateCacheService>();
+                    services.AddTransient<IExchangeRateServiceClient, ExchangeRateServiceClient>();
+                    services.AddTransient<IEventNotificationService, EventNotificationService>();
+
                     var serviceProvider = services.BuildServiceProvider();
-                    serviceProvider.GetRequiredService<IExchangeRateProcessingService>().Process();
+                    serviceProvider.GetRequiredService<IExchangeRateImporter>().Process();
                 });
 
-            await builder.RunConsoleAsync();
+            await host.RunConsoleAsync();
         }
     }
 }
